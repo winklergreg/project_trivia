cfb9f6c093e9ff411380ec74a122e61d4570a516
Greg Winkler <winklergregory@yahoo.com>
2022-09-18 12:04:54 -0400

    Updated

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -80,23 +80,16 @@ def create_app(test_config=None):
 
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
-        question = Question.query.filter(Question.id == question_id).one_or_none()
-
-        if question is None:
-            abort(404)
-
         try:
+            question = Question.query.get(question_id)
             question.delete()
-            selection = Question.query.order_by(Question.id).all()
-            current_questions = paginate_questions(request, selection)
+
+            return jsonify({
+                'success': True,
+                'deleted': question_id
+            })
         except:
             abort(422)
-
-        return jsonify({
-            'success': True,
-            'deleted': question_id,
-            'questions': current_questions
-        })
 
     @app.route('/questions', methods=['POST'])
     def create_question():
@@ -142,15 +135,6 @@ def create_app(test_config=None):
         except:
             abort(404)
 
-
-    """
-    @TODO:
-    Create a GET endpoint to get questions based on category.
-
-    TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
-    category to be shown.
-    """
     @app.route('/categories/<int:category_id>/questions')
     def get_questions_by_category(category_id):
         try:
@@ -169,55 +153,38 @@ def create_app(test_config=None):
         except:
             abort(404)
 
-    """
-    @TODO:
-    Create a POST endpoint to get questions to play the quiz.
-    This endpoint should take category and previous question parameters
-    and return a random questions within the given category,
-    if provided, and that is not one of the previous questions.
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
-    """
     @app.route('/quizzes', methods=['POST'])
     def get_random_quiz_question():
-        body = request.get_json()
+        try:
+            body = request.get_json()
 
-        previous_questions = body.get('previous_questions')
-        category = body.get('quiz_category')
+            previous_questions = body.get('previous_questions', None)
+            category = body.get('quiz_category', None)
 
-        if (category is None) or (previous_questions is None):
+            if category.get('id') == 0:
+                questions = Question.query.all()
+            else:
+                questions = Question.query.filter_by(
+                    category=category.get('id')
+                ).all()
+
+            total_questions = len(questions)
+            question = get_question(questions)
+            while check_if_question_used(question, previous_questions):
+                question = get_question(questions)
+
+                if len(previous_questions) == total_questions:
+                    return jsonify({
+                        'success': True
+                    })
+
+            return jsonify({
+                'success': True,
+                'question': question.format()
+            })
+        except:
             abort(400)
 
-        if category.get('id') == 0:
-            questions = Question.query.all()
-        else:
-            questions = Question.query.filter_by(
-                category=category.get('id')
-            ).all()
-
-        total_questions = len(questions)
-        question = get_question(questions)
-        while check_if_question_used(question, previous_questions):
-            question = get_question(questions)
-
-            if len(previous_questions) == total_questions:
-                return jsonify({
-                    'success': True
-                })
-
-        return jsonify({
-            'success': True,
-            'question': question.format()
-        })
-
-
-    """
-    @TODO:
-    Create error handlers for all expected errors
-    including 404 and 422.
-    """
     @app.errorhandler(400)
     def bad_request(error):
         return(
@@ -225,7 +192,7 @@ def create_app(test_config=None):
                 'success': False,
                 'error': 400,
                 'message': 'bad request'
-            })
+            }), 400
         )
 
     @app.errorhandler(404)
